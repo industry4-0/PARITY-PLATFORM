cb30cba365cfbad9bc41f4d320fd438ac7a3b61a
Alexios Karadimos <karadalex@gmail.com>
2019-11-30 14:57:59 +0200

    user table changes: - added first, last name - vat - role

--- webapp/database/migrations/2014_10_12_000000_create_users_table.php
+++ webapp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('vat_num');
+            $table->enum('role', ['producer', 'buyer']);
             $table->rememberToken();
             $table->timestamps();
         });
